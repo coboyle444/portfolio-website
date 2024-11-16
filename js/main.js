bff5cf03ab0503d45111eefb6c30ce0bed4bfd6c
coboyle444 <courtlyn.oboyle@edu.sait.ca>
2024-11-16 11:29:32 -0700

    fix: broken greeting and date

--- js/main.js
+++ js/main.js
@@ -1,8 +1,38 @@
-alert("Hello, world!");
+
 
 // Footer
-const fYear = document.getElementById("footerYear")
+window.onload = function() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById("footerYear").textContent = currentYear;
+};
+
+
 //Portfolio Greeting
+window.onload = function() {
+    const greetingElement = document.getElementById("greeting");
+    const currentTime = new Date().getHours(); 
+
+    let greetingMessage = "";
+    let greetingStyle = "";
+
+
+    if (currentTime >= 5 && currentTime < 12) {
+        greetingMessage = "Morning sunshine!";
+        greetingStyle = "morning"; 
+    } else if (currentTime >= 12 && currentTime < 18) {
+        greetingMessage = "Afternoon my friend!";
+        greetingStyle = "afternoon"; 
+    } else {
+        greetingMessage = "Evening!";
+        greetingStyle = "evening"; 
+    }
+
+
+    greetingElement.textContent = greetingMessage;
+    greetingElement.className = greetingStyle;
+};
+
+
 // Alert Button
 // Hover Button
 // for Loops
